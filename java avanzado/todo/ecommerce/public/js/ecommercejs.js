f83f7f5cf56638d40ead85e8045034d12b3ab04b
nsimon <noelsimonaguilarj@gmail.com>
2022-01-27 20:39:22 +0100

    Ejercicios 27/1/2021 /2

---
+++ java avanzado/todo/ecommerce/public/js/ecommercejs.js
@@ -0,0 +1,17 @@
+
+
+function miFuncion() {
+  var usuarios01 = document.getElementById("productos");
+  usuarios01.innerHTML = "<li>5</li><li>6</li><li>7</li>";
+
+  var usuarios01 = document.getElementById("mayorgasto");
+  usuarios01.innerHTML = "<li>5</li><li>6</li><li>7</li>";
+
+  var usuarios01 = document.getElementById("mayorcompras");
+  usuarios01.innerHTML = "<li>5</li><li>6</li><li>7</li>";
+  console.log(usuarios01);
+}
+
+function button() {
+  console.log("ALERTA!!");
+}
